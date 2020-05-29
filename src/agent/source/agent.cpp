9e13a91ce3ee3f647645bcd6a6c92047ab568635
Michael Slinkin <michaelnslinkin@gmail.com>
2020-05-29 21:42:50 +0300

    made changes to facilitate boost usage

--- src/agent/source/agent.cpp
+++ src/agent/source/agent.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
+#include <boost/asio.hpp>
+
 int main()
 {
     std::cout << "Hi World, I'm Agent" << std::endl;
     
+    boost::asio::io_service service;
+
     return 0;
 }
