cbd504235bc956bdf0e98713ae1f5496314200ac
Michael Slinkin <michaelnslinkin@gmail.com>
2020-06-11 00:06:15 +0300

    #16 taught agent the arts of UDP

--- src/agent/source/pcConMonAgent.cpp
+++ src/agent/source/pcConMonAgent.cpp
@@ -8,47 +8,32 @@
 #include "hardwareInfoProviderClasses.hpp"
 #include "configModificationProviderClasses.hpp"
 
-class Server
-{
-    private:
-        boost::asio::io_service service;
-        boost::asio::ip::tcp::endpoint endPoint;
-        boost::asio::ip::tcp::acceptor acceptor;
-        std::map<std::string, boost::shared_ptr<boost::asio::ip::tcp::socket>> clients;
-        void processClientSession(boost::shared_ptr<boost::asio::ip::tcp::socket> sock);
-    public:
-        Server();
-        Server(uint16_t port);
-        void listen();
-};
-
 class Agent
 {
     private:
         BasicOperationStateChangeProvider commander;
-        ComputerSystemProvider informer;
         ComputerSystem* thisSystem;
+        ComputerSystemProvider informer;
         boost::asio::io_service service;
-        boost::asio::ip::tcp::endpoint endPoint;
+        boost::asio::ip::tcp::endpoint tcpEndPoint;
         boost::asio::ip::tcp::acceptor acceptor;
+        boost::asio::ip::udp::endpoint udpEndPoint;
         std::map<std::string, boost::shared_ptr<boost::asio::ip::tcp::socket>> clients;
-        void processClientSession(boost::shared_ptr<boost::asio::ip::tcp::socket> sock);
-        void listen();
         void closeAllConnections();
+        void listenTcp();
+        void listenUdp();
+        void processTcpSession(boost::shared_ptr<boost::asio::ip::tcp::socket> sock);
     public:
         Agent();
         Agent(uint16_t port);
         int operate();
 };
 
-void Agent::processClientSession(boost::shared_ptr<boost::asio::ip::tcp::socket> sock)
+void Agent::processTcpSession(boost::shared_ptr<boost::asio::ip::tcp::socket> sock)
 {
-    boost::system::error_code ec;
-    std::string message;
-
     while (true)
     {
-        char data[512] = {};
+        char data[128] = {};
         std::string strData;
 
         try
@@ -58,21 +43,26 @@ void Agent::processClientSession(boost::shared_ptr<boost::asio::ip::tcp::socket>
             if (len > 0)
             {
                 strData = std::string(data);
-                boost::to_upper(data);
+                boost::to_upper(strData);
 
-                if (strData == "IDENTIFY")
+                if (strData == "SEND")
                 {
-                    sock->write_some(boost::asio::buffer(thisSystem->getName()));
+                    json j;
+                    vector<uint8_t> vMsgPack;
+
+                    thisSystem->serialize(j);
+                    vMsgPack = json::to_msgpack(j);
+                    sock->write_some(boost::asio::buffer(vMsgPack));
                 }
                 else if (strData == "REFRESH")
                 {
                     json j;
-                    std::stringstream ss;
+                    vector<uint8_t> vMsgPack;
 
                     informer.scanForManagedElements();
                     thisSystem->serialize(j);
-                    ss << j;
-                    sock->write_some(boost::asio::buffer(ss.str()));
+                    vMsgPack = json::to_msgpack(j);
+                    sock->write_some(boost::asio::buffer(vMsgPack));
                 }
                 else if (strData == "SHUTDOWN")
                 {
@@ -90,6 +80,8 @@ void Agent::processClientSession(boost::shared_ptr<boost::asio::ip::tcp::socket>
         }
         catch (std::exception& e)
         {
+            std::string fullAddress = sock->remote_endpoint().address().to_string() + ":" + std::to_string(sock->remote_endpoint().port());
+            clients.erase(fullAddress);
             sock->close();
             break;
         }
@@ -104,7 +96,7 @@ void Agent::closeAllConnections()
     }
 }
 
-void Agent::listen()
+void Agent::listenTcp()
 {
     while (true)
     {
@@ -115,18 +107,61 @@ void Agent::listen()
         acceptor.accept(*sock);
         fullAddress += sock->remote_endpoint().address().to_string() + ":" + std::to_string(sock->remote_endpoint().port());
         clients.insert({fullAddress, sock});
-        std::thread pThread(&Agent::processClientSession, this, sock);
+        std::thread pThread(&Agent::processTcpSession, this, sock);
         pThread.detach();
     }
 }
 
-Agent::Agent() : commander(), informer(), service(), endPoint(boost::asio::ip::tcp::v4(), 9090), acceptor(service, endPoint)
+void Agent::listenUdp()
+{
+    boost::shared_ptr<boost::asio::ip::udp::socket> sock(new boost::asio::ip::udp::socket(service));
+    boost::asio::ip::udp::endpoint remoteEndPoint(boost::asio::ip::udp::v4(), 9090);
+
+    sock->open(boost::asio::ip::udp::v4());
+    sock->set_option(boost::asio::socket_base::broadcast(true));
+    sock->set_option(boost::asio::socket_base::reuse_address(true));
+    sock->bind(udpEndPoint);
+
+    while (true)
+    {
+        char data[128] = {};
+        std::string strData;
+
+        try
+        {
+            size_t len = sock->receive_from(boost::asio::buffer(data), remoteEndPoint);
+            if (len > 0)
+            {
+                strData = std::string(data);
+                boost::to_upper(strData);
+
+                if (strData == "IDENTIFY")
+                {
+                    sock->send_to(boost::asio::buffer(thisSystem->getName()), remoteEndPoint);
+                }
+                else if (strData == "SHUTDOWN" || strData == "REBOOT")
+                {
+                    sock->close();
+                    break;
+                }
+            }
+        }
+        catch (std::exception& e)
+        {
+            std::cout << "An exception occurred: " << e.what() << std::endl;
+            sock->close();
+            break;
+        }
+    }
+}
+
+Agent::Agent() : commander(), informer(), service(), tcpEndPoint(boost::asio::ip::tcp::v4(), 9090), acceptor(service, tcpEndPoint), udpEndPoint(boost::asio::ip::address_v4::from_string("0.0.0.0"), 9090)
 {
     informer.scanForManagedElements();
     thisSystem = (ComputerSystem*)informer.getManagedElement();
 }
 
-Agent::Agent(uint16_t port) : commander(), informer(), service(), endPoint(boost::asio::ip::tcp::v4(), port), acceptor(service, endPoint)
+Agent::Agent(uint16_t port) : commander(), informer(), service(), tcpEndPoint(boost::asio::ip::tcp::v4(), port), acceptor(service, tcpEndPoint), udpEndPoint(boost::asio::ip::address_v4::from_string("0.0.0.0"), port)
 {
     informer.scanForManagedElements();
     thisSystem = (ComputerSystem*)informer.getManagedElement();
@@ -136,7 +171,9 @@ int Agent::operate()
 {
     int opResult = 0;
 
-    listen();
+    std::thread udpThread(&Agent::listenUdp, this);
+    udpThread.detach();
+    listenTcp();
 
     return opResult;
 }
